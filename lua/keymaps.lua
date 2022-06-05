1d69b20ef5cd2d94ee0b6639f1c034850afcc197
grudshow <31015135+grudshow@users.noreply.github.com>
2022-06-05 03:45:18 +0300

    add lua

---
+++ lua/keymaps.lua
@@ -0,0 +1,54 @@
+local map = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
+
+--Leader key
+map("",",","<Nop>", default_opts)
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+
+-- //INSERT MODE// --
+
+-- Смена esc
+map("i", "jj", "<ESC>", {})
+-- Navigate
+map("i", "<C-h>", "<Left>", default_opts)
+
+-- //NORMAL MODE// --
+--
+-- Сохранение 
+map("n", "<C-s>", "<CR>:w<CR>", default_opts)
+
+-- Window Navigate
+map("n", "<C-h>", "<C-w>h", default_opts)
+map("n", "<C-l>", "<C-w>l", default_opts)
+map("n", "<C-j>", "<C-w>j", default_opts)
+map("n", "<C-k>", "<C-w>k", default_opts)
+
+--Resize with arrow
+map("n", "<C-Up>", ":resize -2<CR>", default_opts)
+map("n", "<C-Down>", ":resize +2<CR>", default_opts)
+map("n", "<C-Left>", ":vertical resize -2<CR>", default_opts)
+map("n", "<C-Right>", ":vertical resize +2<CR>", default_opts)
+
+--Tab switch buffer
+map("n", "<S-l>", ":BufferLineCycleNext<CR>", default_opts)
+map("n", "<S-h>", ":BufferLineCyclePrev<CR>", default_opts)
+
+--Hide highlight search
+map("n", "<leader><space>", ":noh<CR>", default_opts)
+
+
+-- //VISUAL MODE// --
+--
+--Move current line / block
+map("x", "K", ":move '<-2<CR>gv-gv", default_opts)
+map("x", "J", ":move '>+1<CR>gv-gv", default_opts)
+
+-- //TERMINAL MODE// --
+--
+--Terminal window navigation
+map("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+map("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+map("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+map("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
---
+++ lua/options.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+local cmd = vim.cmd
+
+opt.backup = false 
+
+opt.clipboard = "unnamedplus" 
+opt.expandtab = true 
+opt.tabstop = 2 
+opt.mouse = "a"
+opt.shiftwidth = 2 
+opt.smarttab = true 
+opt.swapfile = false 
+opt.writebackup = false 
+opt.autowrite = true 
+opt.hlsearch = true 
+opt.ignorecase = true 
+opt.smartindent = true 
+opt.splitbelow = true 
+opt.splitright = true 
+opt.cursorline = true 
+opt.scrolloff = 8 
+opt.sidescrolloff = 8 
+opt.timeoutlen = 1000
+opt.updatetime = 300
+
+cmd "set whichwrap+=<,>,[,],h,l"
+cmd [[set iskeyword+=-]]
+cmd [[set formatoptions-=cro]]
